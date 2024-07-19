41ad07e4b96d997d9b816af05409e7423cfcf170
Wellington Miguel <wellington2010_silva@hotmail.com>
2024-07-18 23:13:01 -0300

    Adicionada a função de enumeração e conversão de inteiro para romano. Enumeração inacabada e conversão para romanos necessita de testes.

--- lib/desafio_cumbuca.ex
+++ lib/desafio_cumbuca.ex
@@ -4,18 +4,49 @@ defmodule DesafioCumbuca do
     IO.puts("Olá, bem-vindo ao melhor enumerador de reis e rainhas de toda Combúquia!")
     IO.puts("-------------------------------------------------------------------------\n")
     IO.puts("Por favor, insira os nomes dos monarcas, um por linha.\nFinalize apertando Enter duas vezes.\n")
-    nomes = criar_listas([])
-    hello(nomes)
+    nomes = criar_lista([])
+    nomes_ordenados = enumerar(nomes)
+    resultado(nomes_ordenados)
   end
-    defp criar_listas(nomes) do
-      nome = IO.gets("")
-      case String.trim(nome) do
+    defp criar_lista(nomes) do
+      nome = IO.gets("") |> String.trim()
+      case nome do
         "" -> nomes
-        nome_trimado -> criar_listas(nomes ++ [nome_trimado])
+        nome_trimado -> criar_lista(nomes ++ [nome_trimado])
       end
     end
-    def hello(lista) do
-      Enum.each(lista, fn nome -> IO.puts(nome) end)
+    defp enumerar(nomes) do
+      mapa_nomes=%{}
+      nomes |> Enum.reduce([], fn nome, lista_nomes ->
+                if Map.get(mapa_nomes, nome) == nil do
+                   Map.put(mapa_nomes, nome, 1)
+                   lista_nomes ++ ["#{nome} I"]
+                else
+                  enumerador = Map.get(mapa_nomes, nome)
+                  Map.put(mapa_nomes, nome, enumerador + 1)
+                  romano = para_romano(enumerador + 1)
+                  lista_nomes ++ ["#{nome} #{romano}"]
+                end
+              end)
+    end
+    defp para_romano(inteiro) do
+      numero_romanos = [
+        {"M", 1000}, {"CM", 900}, {"D", 500}, {"CD", 400}, {"C", 100},
+        {"XC", 90}, {"L", 50}, {"XL", 40}, {"X", 10}, {"IX", 9},
+        {"V", 5}, {"IV", 4}, {"I", 1}
+      ]
+
+      numero_romanos
+      |> Enum.reduce({inteiro, ""}, fn {simbolo, valor}, {restante, romano} ->
+        contador = div(restante, valor)
+        novo_romano = romano <> String.duplicate(simbolo, contador)
+        novo_restante = restante - valor * contador
+        {novo_restante, novo_romano}
+      end)
+      |> elem(1)
+    end
+    defp resultado(nomes_ordenados) do
+       IO.puts(Enum.join(nomes_ordenados, "\n"))
     end
 end
 
